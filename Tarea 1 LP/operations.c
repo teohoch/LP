0bcbbd67a3f968b9d46d7f749d19a491da155166
teohoch <teohoch2@gmail.com>
2014-04-24 23:59:04 -0400

    Update operations.c

--- Tarea 1 LP/operations.c
+++ Tarea 1 LP/operations.c
@@ -5,44 +5,73 @@
  *      Author: teohoch
  */
 #include "matrix.h"
+#include "operations.h"
+#include <stdio.h>
 
-matrix * add(matrix * a,matrix * b)
-/*source http://www.phanderson.com/C/mat_add.html */
+void printMatrix(matrix * a)
+{
+	printf("\n %c \n", a->id);
+	printf("%d \n", a->dim);
+	int i,j;
+
+	for (i = 0; i < a->dim; i++)
+	{
+		for (j = 0; j < a->dim; j++)
+		{
+			printf("%f   ",(a->data[i][j]));
+		}
+		printf("\n");
+	}
+}
+
+void createMatrix(int size, char name, matrix * novo)
 {
 
+		novo->data= malloc(sizeof(float *));
+		int p, i;
 
-	 int i, j;
+		for(p=0;p<size;p++)
+		{
+			novo->data[p] = (float*)malloc(size*sizeof(float));
+			for(i=0;i<size; i++)
+			{
+				novo->data[p][i]= 0.0;
+			}
+		}
+		novo->dim = size;
+		novo->id = name;
+
+		printMatrix(novo);
+
+
+}
+
+matrix * add(matrix * a,matrix * b)
+{
 	 int sizeA = a->dim;
 	 int sizeB = b->dim;
 
-
 	 if (sizeA == sizeB)
 	 {
-		 /* Inicializar matriz de resultado */
-		 matrix *resultado;
-		 resultado = (matrix *)mallock(sizeof(matrix));
-		 resultado->id = "r";
-		 resultado->dim = sizeA;
-		 float data[sizeA][sizeA] = {0};
-		 resultado->data = &data;
+		 matrix resultado;
+		 createMatrix(sizeA,'r',&resultado);
 
-		 /* Comenzar calculo*/
+		 int i, j;
 		 for(i=0; i<sizeA; i++)
 		 {
 			 for(j=0; j<sizeA; j++)
 			 {
-				 resultado->data[i][j] = a->data[i][j] + b->data[i][j];
+				 resultado.data[i][j] = a->data[i][j] + b->data[i][j];
 			 }
-		   }
-		 return resultado;
+		 }
+		 printMatrix(&resultado);
 	 }else
 		 return NULL;
+
 }
 
 matrix * mult(matrix * a,matrix * b)
 {
-
-
 	 int i, j;
 	 int sizeA = a->dim;
 	 int sizeB = b->dim;
@@ -51,23 +80,26 @@ matrix * mult(matrix * a,matrix * b)
 	 if (sizeA == sizeB)
 	 {
 		 /* Inicializar matriz de resultado */
-		 matrix *resultado;
-		 resultado = (matrix *)mallock(sizeof(matrix));
-		 resultado->id = "r";
-		 resultado->dim = sizeA;
-		 float data[sizeA][sizeA] = {0};
-		 resultado->data = &data;
+		 matrix resultado;
+		 createMatrix(sizeA,'r',&resultado);
 
 		 /* Comenzar calculo*/
+		 int i, j, k;
 		 for(i=0; i<sizeA; i++)
 		 {
 			 for(j=0; j<sizeA; j++)
 			 {
-				 /*TODO Falta un for aqui para el calculo de multiplicacion*/
+				 float cell = 0;
+				 for(k = 0; k < sizeA; k++)
+				/*cambiando estos for puedo convertirlo para que no sea nesesario que las matrices sean cuadradas*/
+				 {
+					 cell = cell + ((a->data[i][k]) * (b->data[k][j]));
+				 }
+				 resultado.data[i][j] = cell;
 			 }
 		   }
-		 return resultado;
+		 printMatrix(&resultado);
+		 return &resultado;
 	 }else
 		 return NULL;
 }
-
