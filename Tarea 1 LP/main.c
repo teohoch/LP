f928ea1e6b14d1c484d8828d7c1b89d62edb831c
teohoch <teohoch2@gmail.com>
2014-04-24 23:54:53 -0400

    Update main.c

--- Tarea 1 LP/main.c
+++ Tarea 1 LP/main.c
@@ -4,10 +4,19 @@
  *  Created on: Apr 9, 2014
  *      Author: teohoch
  */
-
+#include <stdio.h>
 #include "matrix.h"
+#include "operations.h"
 
-main(void)
+main()
 {
+	int nMatris;
+	char *ruta = "matrices.lp"; /*TODO reemplazar despues para permitir pasar este argumento por consola*/
+	FILE *file = fopen( ruta, "r" );
+	matrix **matrices;
+	matrices = read(file, &nMatris);
+	mult(matrices[0],matrices[1]);
+
+
 
 }
